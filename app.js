0ccf10a8ef35275ad9b7a51c0cb212bce6831de8
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2021-12-29 13:00:07 +0530

    added yarg pkg and created add remove list and read commands

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 //const fs = require('fs');
 //const validator  = require('validator');
 //import  chalk  from 'chalk';
-const chalk = require('chalk')
+//const chalk = require('chalk')
+const yargs = require('yargs')
+const { hideBin } = require('yargs/helpers')
 //const { getNotes } = require('./notes')
 //fs.writeFileSync('jassi.txt', 'lal la al lala laa');
 //fs.appendFileSync('note.pdf', 'I am starting backend development!')
@@ -15,4 +17,48 @@ const chalk = require('chalk')
 //console.log(validator.isEmail('jir212singh.com')) // false
 
 
-console.log(chalk.red('Error!'))
+//console.log(chalk.red('Error!'))
+
+//console.log(yargs(hideBin(process.argv)).argv)
+
+// Working Example  =>
+
+//add
+yargs(hideBin(process.argv)).command({
+    command:'add',
+    describe:'Adding command',
+    handler:function(){
+        console.log('Adding notes')
+    }
+}).parse()
+
+
+//remove
+yargs(hideBin(process.argv)).command({
+    command:'remove',
+    describe:'remove command',
+    handler:function(){
+        console.log('removing notes')
+    }
+}).parse()
+
+
+// read
+yargs(hideBin(process.argv)).command({
+    command:'read',
+    describe:'Read command',
+    handler:function(){
+        console.log('Reading notes')
+    }
+}).parse()
+
+
+//list
+
+yargs(hideBin(process.argv)).command({
+    command:'list',
+    describe:'Listing command',
+    handler:function(){
+        console.log('Listing notes')
+    }
+}).parse()
