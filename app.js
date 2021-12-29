338a282e777f8318913126656fdd7e7b1725a5a5
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2021-12-29 10:43:13 +0530

    installed chalk pkg and printed success

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // @ts-nocheck
-const fs = require('fs');
-const validator  = require('validator')
-const { getNotes } = require('./notes')
+//const fs = require('fs');
+//const validator  = require('validator');
+//import  chalk  from 'chalk';
+const chalk = require('chalk')
+//const { getNotes } = require('./notes')
 //fs.writeFileSync('jassi.txt', 'lal la al lala laa');
 //fs.appendFileSync('note.pdf', 'I am starting backend development!')
 
 // @ts-ignore
-console.log(getNotes());
+//console.log(getNotes());
 
 // @ts-ignore
-console.log(validator.isEmail('jasbir212singh@gmail.com')) // true
-console.log(validator.isEmail('jir212singh.com')) // false
+//console.log(validator.isEmail('jasbir212singh@gmail.com')) // true
+//console.log(validator.isEmail('jir212singh.com')) // false
+
+
+console.log(chalk.green('Success!'))
