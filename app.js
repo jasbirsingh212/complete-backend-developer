9d8d8e0e3a13e28be9472bbcf4fd1057aea31cfa
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2021-12-29 10:04:54 +0530

    added package.json and installed validator pkg

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+// @ts-nocheck
 const fs = require('fs');
+const validator  = require('validator')
 const { getNotes } = require('./notes')
 //fs.writeFileSync('jassi.txt', 'lal la al lala laa');
 //fs.appendFileSync('note.pdf', 'I am starting backend development!')
 
 // @ts-ignore
 console.log(getNotes());
+
+// @ts-ignore
+console.log(validator.isEmail('jasbir212singh@gmail.com')) // true
+console.log(validator.isEmail('jir212singh.com')) // false
