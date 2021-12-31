eb78d49f4d4ddbff7e2dc4c997ce0a12e545b59d
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2021-12-31 11:47:05 +0530

    setting up CRUD function

--- notes.js
+++ notes.js
@@ -1,7 +1,30 @@
 const getNotes = () => 'your notes';
 
+const addNote = ( title, body ) => {
+
+    console.log(title, body)
+
+}
+
+const readNote = ( title) => {
+    console.log(title)
+}
+
+const removeNote = ( title ) => {
+    console.log(title)
+
+}
+
+const listNotes = () => {
+console.log('listing notes')
+
+}
 
 module.exports={
-    getNotes
+    getNotes,
+    addNote,
+    readNote,
+    removeNote,
+    listNotes,
 }
 
