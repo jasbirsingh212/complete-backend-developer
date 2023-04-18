99a8d559b60b50c99b394665c17cd0087b380483
Jasbir Singh <jasbir212singh@gmail.com>
2023-04-18 09:56:40 +0530

    installed npm package validator to validate email and urls

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,7 +4,13 @@
 
 // fs.appendFileSync("notes.txt", " Learning Node for backend development.");
 
-
 // const { getNotes } = require('./notes');
 
 // getNotes()
+
+// const validator = require("validator");
+
+// console.log(validator.isEmail("jasbir212singh@gmail.com"));
+// console.log(validator.isEmail("jasbir212.com"));
+// console.log(validator.isURL("jasbir.com"));
+// console.log(validator.isURL("jasbir212singh@gmail"));
