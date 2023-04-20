2b9343cdf09736c733f5f60f50fee7a6e47cca2f
Jasbir Singh <jasbir212singh@gmail.com>
2023-04-20 13:49:32 +0530

    completed notes-app

---
+++ notes-app/learnings.js
@@ -0,0 +1,8 @@
+// const book = {
+//     title: "My title",
+//     body: "my body"
+// }
+
+// const bookJSON = JSON.stringify(book);
+
+// console.log(bookJSON["title"]) con not get property like that
