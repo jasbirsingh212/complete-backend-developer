a900f5a2b4b108053270374b8a85bf38fe5faa2d
Jasbir Singh <jasbir212singh@gmail.com>
2023-04-23 11:44:00 +0530

    removed unwanted console log

--- weather-app/index.js
+++ weather-app/index.js
@@ -6,27 +6,25 @@ const getData = async (url, params) => {
   });
 };
 
-console.log(process.env);
+
 const params1 = {
   q: "New Delhi",
   limit: "1",
   appid: `${process.env.APP_ID}`,
 };
 
-getData(process.env.GEOCOING_URL, params1).then(async(geolocationData) => {
+getData(process.env.GEOCOING_URL, params1)
+  .then(async (geolocationData) => {
+    const params = {
+      lat: `${geolocationData?.data[0]?.lat}`,
+      lon: `${geolocationData?.data[0]?.lon}`,
+      appid: `${process.env.APP_ID}`,
+      units: `metric`,
+    };
 
-  const params = {
-  lat: `${geolocationData?.data[0]?.lat}`,
-  lon: `${geolocationData?.data[0]?.lon}`,
-  appid: `${process.env.APP_ID}`,
-  units: `metric`,
-};
-
-const weatherData = await getData(process.env.WEATHER_URL, params);
-console.log(weatherData?.data);
-
-}).catch((e) => {
-    console.log(e)
-});
-
-
+    const weatherData = await getData(process.env.WEATHER_URL, params);
+    console.log(weatherData?.data);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
