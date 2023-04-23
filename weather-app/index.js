1fd95139bd5206efc3e2102bf5234b09eb0e658b
Jasbir Singh <jasbir212singh@gmail.com>
2023-04-23 11:32:55 +0530

    getting the latittude and longittude and then current weather info from open weather api

---
+++ weather-app/index.js
@@ -0,0 +1,32 @@
+const axios = require("axios").default;
+require("dotenv").config();
+const getData = async (url, params) => {
+  return await axios.get(url, {
+    params,
+  });
+};
+
+console.log(process.env);
+const params1 = {
+  q: "New Delhi",
+  limit: "1",
+  appid: `${process.env.APP_ID}`,
+};
+
+getData(process.env.GEOCOING_URL, params1).then(async(geolocationData) => {
+
+  const params = {
+  lat: `${geolocationData?.data[0]?.lat}`,
+  lon: `${geolocationData?.data[0]?.lon}`,
+  appid: `${process.env.APP_ID}`,
+  units: `metric`,
+};
+
+const weatherData = await getData(process.env.WEATHER_URL, params);
+console.log(weatherData?.data);
+
+}).catch((e) => {
+    console.log(e)
+});
+
+
