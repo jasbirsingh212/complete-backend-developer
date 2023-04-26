21e39067648ce494785b184192e0cac663a08ec4
Jasbir Singh <jasbir212singh@gmail.com>
2023-04-26 17:19:12 +0530

    created public folder with html file: about, help, index

--- web-server/src/index.js
+++ web-server/src/index.js
@@ -2,17 +2,17 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
-app.get(``, (req, res) => {
-  res.send("Home Page");
-});
+// app.get(``, (req, res) => {
+//   res.send("Home Page");
+// });
 
-app.get(`/help`, (req, res) => {
-  res.send("Help Page");
-});
+// app.get(`/help`, (req, res) => {
+//   res.send("Help Page");
+// });
 
-app.get(`/about`, (req, res) => {
-  res.send("About Page");
-});
+// app.get(`/about`, (req, res) => {
+//   res.send("About Page");
+// });
 
 app.get(`/weather`, (req, res) => {
   res.send("Weather Page");
