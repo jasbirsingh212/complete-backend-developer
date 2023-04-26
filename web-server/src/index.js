7df79269d441817ce9032875dc77a9b223f95d20
Jasbir Singh <jasbir212singh@gmail.com>
2023-04-26 16:43:54 +0530

    added home about weather help routes

--- web-server/src/index.js
+++ web-server/src/index.js
@@ -2,6 +2,22 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
+app.get(``, (req, res) => {
+  res.send("Home Page");
+});
+
+app.get(`/help`, (req, res) => {
+  res.send("Help Page");
+});
+
+app.get(`/about`, (req, res) => {
+  res.send("About Page");
+});
+
+app.get(`/weather`, (req, res) => {
+  res.send("Weather Page");
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server runing on port ${port}`);
